cb4b1247ffd518b07c3d7ff97f671676bcc24cd3
Guilhermecodemaker <guilhermehenrickdireito@gmail.com>
2023-12-03 13:07:04 -0300

    refactor

--- ex0028 JOGO DE ADVINHAÇÃO.py
+++ ex0028 JOGO DE ADVINHAÇÃO.py
@@ -1,13 +1,14 @@
 from random import choice
 from time import sleep
-print('=-'*30)
-print('Tente adivinhar o numero que o computador vai pensar!')
+print('\033[35m -=-'* 30)
+print('\033[mTente adivinhar o numero que o computador vai pensar!')
 n = int(input('Escolha um número de 1 a 5: '))
 s = choice([1,2,3,4,5])
-sleep(3)
+sleep(2)
 if n == s:
-    print('você ACERTOU PARABÉNS!!!!!')
+    print('\033[34mvocê ACERTOU PARABÉNS!!!!! \033[m')
 else:
-    print('você errou o numero escolhido foi {}'.format(s))
+    print('\033[31mvocê ERROU o numero escolhido foi {} \033[m'.format(s))
 
-print('=-'*30)
+print('\033[35m -=-' *30)
+
